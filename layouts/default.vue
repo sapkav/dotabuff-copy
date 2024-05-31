46836856a4df9c6d7be18fc9540a9f713216edfd
sapkav <69104777+sapkav@users.noreply.github.com>
2024-05-31 13:37:14 +0300

    start

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="shadow-lg bg-white pl-5 pt-4 pb-4 gap-5 round flex">
+    <div class="flex container gap-5 w-full">
+      <NuxtLink to="/" class="btn">Home</NuxtLink>
+      <NuxtLink to="/esport" class="btn">Esports</NuxtLink>
+      <NuxtLink to="/heroes" class="btn">Heroes</NuxtLink>
+      <NuxtLink to="/items" class="btn">Items</NuxtLink>
+      <NuxtLink to="/players" class="btn">Players</NuxtLink>
+      <NuxtLink to="/mathces" class="btn">Matches</NuxtLink>
+      <NuxtLink to="/blog" class="btn">Blog</NuxtLink>
+      <NuxtLink to="/forums" class="btn">Forums</NuxtLink>
+      <NuxtLink to="/plus" class="btn">Plus</NuxtLink>
+    </div>
+    <div class="container mx-auto p-4 text-blue-700">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+</style>
